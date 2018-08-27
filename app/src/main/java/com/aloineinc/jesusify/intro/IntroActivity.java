ffdfc0cdb473382754097b9af5e8b94683cd2ffe
Tobiloba Adejumo <eniolaplus08@gmail.com>
2018-08-27 15:57:18 +0100

    First commit & login and signup layout created

---
+++ app/src/main/java/com/aloineinc/jesusify/intro/IntroActivity.java
@@ -0,0 +1,14 @@
+package com.aloineinc.jesusify.intro;
+
+import android.os.Bundle;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+public class IntroActivity extends AppIntro {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
