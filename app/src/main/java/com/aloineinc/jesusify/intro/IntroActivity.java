b9f9b1603c15c8c031103b5683f679dfa17480e2
Tobiloba Adejumo <eniolaplus08@gmail.com>
2018-08-28 13:48:53 +0100

    Intro fixed

--- app/src/main/java/com/aloineinc/jesusify/intro/IntroActivity.java
+++ app/src/main/java/com/aloineinc/jesusify/intro/IntroActivity.java
@@ -1,14 +1,78 @@
 package com.aloineinc.jesusify.intro;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+;
 
+import com.aloineinc.jesusify.R;
 import com.github.paolorotolo.appintro.AppIntro;
 
-public class IntroActivity extends AppIntro {
+public class IntroActivity extends AppCompatActivity {
+    private ViewPager mPager;
+    private int[] layouts = {R.layout.firstslide,R.layout.secondslide,R.layout.thirdslide};
+    private MpagerAdapter mpagerAdapter;
+    private LinearLayout Dots_Layout;
+    private ImageView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        init();
+        createDots(0);
+        pageListener();
+
+    }
+
+    private void init() {
+        mPager = findViewById(R.id.viewPager);
+        mpagerAdapter = new MpagerAdapter(layouts,this);
+        mPager.setAdapter(mpagerAdapter);
+        Dots_Layout = findViewById(R.id.dotsLayout);
+    }
+
+    private void createDots(int current_position) {
+        if (Dots_Layout != null) {
+            Dots_Layout.removeAllViews();
+        }
+        dots = new ImageView[layouts.length];
+        for (int i = 0; i < layouts.length; i++) {
+            dots[i] = new ImageView(this);
+            if (i == current_position) {
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.active_dots));
+
+            } else {
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.inactive_dots));
+            }
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.setMargins(4, 0, 4, 0);
+
+            Dots_Layout.addView(dots[i], params);
+        }
+    }
+    private void pageListener() {
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                createDots(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
     }
 }
