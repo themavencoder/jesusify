8311a30fc26bc9290b6dbf9c9b95eedb80252450
Tobiloba Adejumo <eniolaplus08@gmail.com>
2018-08-29 09:17:48 +0100

    App in good condition

--- app/src/main/java/com/aloineinc/jesusify/intro/IntroActivity.java
+++ app/src/main/java/com/aloineinc/jesusify/intro/IntroActivity.java
@@ -1,9 +1,12 @@
 package com.aloineinc.jesusify.intro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,15 +15,16 @@ import android.widget.TextView;
 ;
 
 import com.aloineinc.jesusify.R;
+import com.aloineinc.jesusify.authentication.SigninActivity;
+import com.aloineinc.jesusify.authentication.SignupActivity;
 import com.github.paolorotolo.appintro.AppIntro;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity  {
     private ViewPager mPager;
     private int[] layouts = {R.layout.firstslide,R.layout.secondslide,R.layout.thirdslide};
     private MpagerAdapter mpagerAdapter;
     private LinearLayout Dots_Layout;
     private ImageView[] dots;
-    private Button mSignUp, mSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,7 @@ public class IntroActivity extends AppCompatActivity {
         mpagerAdapter = new MpagerAdapter(layouts,this);
         mPager.setAdapter(mpagerAdapter);
         Dots_Layout = findViewById(R.id.dotsLayout);
+
     }
 
     private void createDots(int current_position) {
@@ -79,4 +84,6 @@ public class IntroActivity extends AppCompatActivity {
     }
 
 
-}
+
+    }
+
