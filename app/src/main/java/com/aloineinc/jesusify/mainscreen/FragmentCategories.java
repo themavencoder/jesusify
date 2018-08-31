5ce1496f3569759bfc3abdd0d807afcf6fd1c49b
Tobiloba Adejumo <eniolaplus08@gmail.com>
2018-08-31 11:23:20 +0100

    Final commit before first merging

--- app/src/main/java/com/aloineinc/jesusify/mainscreen/FragmentCategories.java
+++ app/src/main/java/com/aloineinc/jesusify/mainscreen/FragmentCategories.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.aloineinc.jesusify.R;
+import com.aloineinc.jesusify.mainscreen.categories.CategoryAdapter;
+import com.aloineinc.jesusify.mainscreen.categories.CategoryModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentCategories extends Fragment {
     View v;
+    private RecyclerView recyclerView;
+    private CategoryAdapter categoryAdapter;
+    private List<CategoryModel> categoryList = new ArrayList<>();
 
     public FragmentCategories() {
 
@@ -20,7 +30,59 @@ public class FragmentCategories extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.categories_fragment,container,false);
+        v = inflater.inflate(R.layout.categories_fragment, container, false);
+        recyclerView = v.findViewById(R.id.recycler_view_mainscreen_categories);
+        categoryAdapter = new CategoryAdapter(getContext(), categoryList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(categoryAdapter);
+
         return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        categoryList.add(new CategoryModel("Faith"));
+        categoryList.add(new CategoryModel("Love"));
+        categoryList.add(new CategoryModel("Peace"));
+        categoryList.add(new CategoryModel("Joy"));
+        categoryList.add(new CategoryModel("Patience"));
+        categoryList.add(new CategoryModel("Faith"));
+        categoryList.add(new CategoryModel("Love"));
+        categoryList.add(new CategoryModel("Peace"));
+        categoryList.add(new CategoryModel("Joy"));
+        categoryList.add(new CategoryModel("Patience"));
+        categoryList.add(new CategoryModel("Faith"));
+        categoryList.add(new CategoryModel("Love"));
+        categoryList.add(new CategoryModel("Peace"));
+        categoryList.add(new CategoryModel("Joy"));
+        categoryList.add(new CategoryModel("Patience"));
+        categoryList.add(new CategoryModel("Faith"));
+        categoryList.add(new CategoryModel("Love"));
+        categoryList.add(new CategoryModel("Peace"));
+        categoryList.add(new CategoryModel("Joy"));
+        categoryList.add(new CategoryModel("Patience"));
+        categoryList.add(new CategoryModel("Faith"));
+        categoryList.add(new CategoryModel("Love"));
+        categoryList.add(new CategoryModel("Peace"));
+        categoryList.add(new CategoryModel("Joy"));
+        categoryList.add(new CategoryModel("Patience"));
+        categoryList.add(new CategoryModel("Faith"));
+        categoryList.add(new CategoryModel("Love"));
+        categoryList.add(new CategoryModel("Peace"));
+        categoryList.add(new CategoryModel("Joy"));
+        categoryList.add(new CategoryModel("Patience"));
+        categoryList.add(new CategoryModel("Faith"));
+        categoryList.add(new CategoryModel("Love"));
+        categoryList.add(new CategoryModel("Peace"));
+        categoryList.add(new CategoryModel("Joy"));
+        categoryList.add(new CategoryModel("Patience"));
+        categoryList.add(new CategoryModel("Faith"));
+        categoryList.add(new CategoryModel("Love"));
+        categoryList.add(new CategoryModel("Peace"));
+        categoryList.add(new CategoryModel("Joy"));
+        categoryList.add(new CategoryModel("Patience"));
+
+
     }
 }
--- app/src/main/java/com/aloineinc/jesusify/mainscreen/categories/CategoryAdapter.java
+++ app/src/main/java/com/aloineinc/jesusify/mainscreen/categories/CategoryAdapter.java
@@ -2,12 +2,16 @@ package com.aloineinc.jesusify.mainscreen.categories;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.aloineinc.jesusify.R;
 
 import java.util.List;
 
-public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyViewHolder>{
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyViewHolder> {
     private Context mContext;
     private List<CategoryModel> categoryList;
 
@@ -18,22 +22,33 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyView
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_mainscreen_categories, parent, false);
+        return new MyViewHolder(v);
+
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.categoryText.setText(categoryList.get(position).getName());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return categoryList.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView categoryText;
+
+
         public MyViewHolder(View itemView) {
+
+
             super(itemView);
+            categoryText = itemView.findViewById(R.id.tv_categorytext);
         }
     }
 
