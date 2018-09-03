e9e4654fd87bf5f5cb02bb3a1bc50a9682fb4faa
Tobiloba Adejumo <eniolaplus08@gmail.com>
2018-09-03 12:56:09 +0100

    Payment layout

---
+++ app/src/main/java/com/aloineinc/jesusify/payment/PaymentActivity.java
@@ -0,0 +1,15 @@
+package com.aloineinc.jesusify.payment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aloineinc.jesusify.R;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
